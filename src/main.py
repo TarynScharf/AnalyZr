5f7bb0f04e58d77f5f0505f8afca9afbe374174d
Taryn Estelle Scharf <20023951@curtin.edu.au>
2021-05-21 14:12:40 +0800

    Redoing previous commit

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from src.view.application_gui import Application
+from src.view.application_view import View
 from src.model.application_model import Model
 from tkinter import Tk
 
 root = Tk()
 model = Model()
-view = Application(root,model)
+view = View(root, model)
 
 #model.set_source_folder_paths('/home/matthew/Code/ZirconSeparation/test/files','/home/matthew/Code/ZirconSeparation/test/files')
 model.set_source_folder_paths('C:/Users/20023951/PycharmProjects/ZirconSeparation/test/files','C:/Users/20023951/PycharmProjects/ZirconSeparation/test/files')
---
+++ src/view/data_capture_dialog.py
@@ -0,0 +1,91 @@
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+
+class DataCaptureDialog():
+    def __init__(self, view):
+        self.view = view
+        self.browse_for_files_window = Toplevel(self.view.master)
+        self.browse_for_files_window.title("Select Images for Spot Capture")
+        self.browse_for_files_window.minsize(400, 100)
+        self.browse_for_files_window.lift()
+
+        #VARIABLES
+        self.json_folder_path = tk.StringVar()
+        self.json_folder_path.set('')
+
+        self.load_json_folder = IntVar()
+
+        self.create_json_var = IntVar()
+
+        self.Folder_Location = tk.StringVar()
+        self.Folder_Location.set('')
+
+        self.image_folder_path = tk.StringVar()
+        self.image_folder_path.set('')
+
+        # browse for images
+        self.image_folder_Label = Label(self.browse_for_files_window, text="Image Folder")
+        self.image_folder_Label.grid(column=0, row=0,sticky = 'w')
+
+        self.image_folder_text_box = Entry(self.browse_for_files_window, width=150, textvariable=self.image_folder_path)
+        self.image_folder_text_box.grid(column=1,columnspan =5, row=0,sticky = 'w')
+
+        self.browse_for_image_folder = Button(self.browse_for_files_window, text="...", width=5, command=lambda: self.Browse('capture', self.browse_for_files_window))
+        self.browse_for_image_folder.grid(column=6, row=0, padx=2, pady=5,sticky = 'w')
+
+        # browse for json files
+        self.load_json_folder_check_button = Checkbutton(self.browse_for_files_window, text='load jsons separately', variable=self.load_json_folder, command=lambda: self.activate_browse_for_json_folder())
+        self.load_json_folder_check_button.grid(column=0, columnspan=2, row=2, padx=2, pady=5, sticky='w')
+
+        self.json_folder_Label = Label(self.browse_for_files_window, text="Json Folder")
+        self.json_folder_Label.config(state=DISABLED)
+        self.json_folder_Label.grid(column=0, row=3,sticky = 'w')
+
+        self.json_folder_text_box = Entry(self.browse_for_files_window, width=150, textvariable=self.json_folder_path)
+        self.json_folder_text_box.config(state=DISABLED)
+        self.json_folder_text_box.grid(column=1,columnspan =5, row=3,sticky = 'w')
+
+        self.browse_for_json_folder = Button(self.browse_for_files_window, text="...", width=5, command=lambda: self.Browse('json',self.browse_for_files_window))
+        self.browse_for_json_folder.config(state=DISABLED)
+        self.browse_for_json_folder.grid(column=6, row=3, padx=2, pady=5,sticky = 'w')
+
+        self.create_json_files_check_button = Checkbutton(self.browse_for_files_window, text='Generate json files if missing', variable=self.create_json_var)
+        self.create_json_files_check_button.grid(column = 0,columnspan=2, row=4, padx=2, pady=5,sticky = 'w')
+
+        self.ok_load_files_for_capture = Button(self.browse_for_files_window, text="OK", width=5, command=lambda: self.load_files())
+        self.ok_load_files_for_capture.grid(column=0, row=5, padx=2, pady=5,sticky = 'w')
+
+        self.cancel_load_files_for_capture = Button(self.browse_for_files_window, text="Cancel", width=8, command=lambda: self.view.close_window(self.browse_for_files_window))
+        self.cancel_load_files_for_capture.grid(column=1, row=5, padx=2, pady=5,sticky = 'w')
+
+    def Browse(self,case_type,window):
+        if case_type == 'json':
+            json_folder_name = self.view.Browse(case_type,window)
+            self.json_folder_location = json_folder_name
+            self.json_folder_path.set(json_folder_name)
+            window.lift()
+
+        elif case_type == 'capture':
+            filename = self.view.Browse(case_type,window)
+            self.image_folder_path.set(filename)
+            if self.load_json_folder.get() == 0:
+                self.json_folder_path.set(self.image_folder_path.get())
+            window.lift()
+
+    def activate_browse_for_json_folder(self):
+        if self.load_json_folder.get() == 1:
+            state = NORMAL
+            path = ""
+        elif self.load_json_folder.get() == 0:
+            state = DISABLED
+            path = self.image_folder_path.get()
+
+        self.json_folder_Label.config(state=state)
+        self.json_folder_text_box.config(state=state)
+        self.browse_for_json_folder.config(state=state)
+        self.view.model.set_json_folder_path(path)
+
+    def load_files(self):
+        self.view.load_files(self.image_folder_path.get(),self.json_folder_path.get())
+        self.browse_for_files_window.destroy()
